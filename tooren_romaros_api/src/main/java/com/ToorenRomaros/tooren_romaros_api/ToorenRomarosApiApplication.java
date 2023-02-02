bf091cb4caeb78e902c6042d75aff9e34a7a136e
jorge-cobos <cobos1932@gmail.com>
2023-02-01 23:24:10 -0600

    created a project with spring initializr

---
+++ tooren_romaros_api/src/main/java/com/ToorenRomaros/tooren_romaros_api/ToorenRomarosApiApplication.java
@@ -0,0 +1,13 @@
+package com.ToorenRomaros.tooren_romaros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToorenRomarosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToorenRomarosApiApplication.class, args);
+	}
+
+}
---
+++ tooren_romaros_api/src/test/java/com/ToorenRomaros/tooren_romaros_api/ToorenRomarosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ToorenRomaros.tooren_romaros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToorenRomarosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
