9a963e90d8d72bd094d3515edabcc4145587a275
Jorge Cobos <cobos1932@gmail.com>
2023-10-19 06:59:29 -0600

    Learning criteria queries

---
+++ api/src/main/java/com/ToorenRomaros/api/dao/FilmSearchDao.java
@@ -0,0 +1,7 @@
+package com.ToorenRomaros.api.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FilmSearchDao {
+}
--- api/src/main/java/com/ToorenRomaros/api/services/CommentService.java
+++ api/src/main/java/com/ToorenRomaros/api/services/CommentService.java
@@ -1,4 +1,16 @@
 package com.ToorenRomaros.api.services;
 
+import com.ToorenRomaros.api.dto.publication.CommentDto;
+import com.ToorenRomaros.api.dto.socials.SocialGenericDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface CommentService {
+    CommentDto createComment(CommentDto commentDto);
+    CommentDto getCommentById(UUID id);
+    List<CommentDto> getAllCommentByUserId(UUID id);
+    List<CommentDto> getAllCommentByFilmId(UUID id);
+    CommentDto updateComment(UUID id, CommentDto commentDto);
+    void deleteComment(UUID id);
 }
