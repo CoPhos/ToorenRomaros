bbde9020d00ecf2cb602ce1be0ffb40ceb3c8602
Jorge Cobos <cobos1932@gmail.com>
2024-01-01 13:35:38 -0600

    added data for tv shows, film genres, film staff

--- api/src/main/java/com/ToorenRomaros/api/entities/publication/CommentEntity.java
+++ api/src/main/java/com/ToorenRomaros/api/entities/publication/CommentEntity.java
@@ -6,7 +6,6 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -22,12 +21,11 @@ public class CommentEntity{
     @GeneratedValue(generator = "uuid4")
     @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
-    @Column(name = "BODY", columnDefinition = "VARCHAR(255)")
+    @Column(name = "BODY", columnDefinition = "VARCHAR(max)")
     @NotNull(message = "Body can not be null")
     private String body;
     @Column(name = "PUBLICATION_DATETIME", columnDefinition = "DATETIME")
-    @FutureOrPresent(message = "Publication date can not be past")
-    private LocalDateTime publicationDateTime;
+    private LocalDateTime postDateTime;
     @Column(name = "LIKE_COUNT", columnDefinition = "VARCHAR(10) DEFAULT 0")
     @Min(value = 0, message = "Like count can not be negative")
     private int likeCount;
@@ -43,55 +41,42 @@ public class CommentEntity{
     public UUID getId() {
         return id;
     }
-
     public void setId(UUID id) {
         this.id = id;
     }
-
     public String getBody() {
         return body;
     }
-
     public void setBody(String body) {
         this.body = body;
     }
-
     public LocalDateTime getPublicationDateTime() {
-        return publicationDateTime;
+        return postDateTime;
     }
-
-    public void setPublicationDateTime(LocalDateTime publicationDateTime) {
-        this.publicationDateTime = publicationDateTime;
+    public void setPublicationDateTime(LocalDateTime postDateTime) {
+        this.postDateTime = postDateTime;
     }
-
     public int getLikeCount() {
         return likeCount;
     }
-
     public void setLikeCount(int likeCount) {
         this.likeCount = likeCount;
     }
-
     public boolean isReported() {
         return reported;
     }
-
     public void setReported(boolean reported) {
         this.reported = reported;
     }
-
     public UserEntity getUser() {
         return user;
     }
-
     public void setUser(UserEntity user) {
         this.user = user;
     }
-
     public FilmEntity getFilm() {
         return film;
     }
-
     public void setFilm(FilmEntity film) {
         this.film = film;
     }
