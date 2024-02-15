fae9b19684f38b963c2d0845015fc499eb74a5b1
Jorge Cobos <cobos1932@gmail.com>
2024-02-15 13:07:14 -0600

    fixed link props and search params for Home component

--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -17,12 +17,12 @@ function Home() {
 
             <TittleCard
                 tittle={'NEW & UPCOMING MOVIES IN THEATERS'}
-                to={'/adasd'}
+                to={'/browse?at=theaters&filmType=all&sortBy=added_date-Desc'}
                 linkText={'View all'}
             ></TittleCard>
             <RegularCarouselManager
                 parameters={
-                    '?atTheaters=1&orderBy=added_date-asc&page=0&size=15'
+                    '?atTheaters=1&genres=&suitableFor=&streamId=&orderBy=added_date-Desc&size=15'
                 }
                 queryName={'NewMovies'}
                 promiseName={'NewMoviesImages'}
@@ -30,12 +30,12 @@ function Home() {
 
             <TittleCard
                 tittle={'NEW & UPCOMING TV SERIES'}
-                to={'/adasd'}
+                to={'/browse?at=home&filmType=2&sortBy=added_date-Desc'}
                 linkText={'View all'}
             ></TittleCard>
             <RegularCarouselManager
                 parameters={
-                    '?filmType=2&atStreaming=1&orderBy=added_date-asc&page=0&size=15'
+                    '?atStreaming=1&filmType=2&genres=&suitableFor=&streamId=&orderBy=added_date-Desc&size=15'
                 }
                 queryName={'NewTvSeries'}
                 promiseName={'NewTvSeriesImages'}
@@ -44,53 +44,57 @@ function Home() {
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-4 gap-x-2 lg:gap-y-4 lg:gap-x-[128px] mb-3">
                 <TopFilmsManager
                     tittle={'MOST POPULAR TV ON RT'}
-                    to={'/browse?filmType=all&sortBy=view_count-Desc'}
+                    to={'/browse?at=home&filmType=all&sortBy=view_count-Desc'}
                     linkText={'View all'}
                     parameters={
-                        '?filmType=all&sortBy=view_count-Desc&page=0&size=10'
+                        '?atStreaming=1&genres=&suitableFor=&streamId=&orderBy=view_count-Desc&size=10'
                     }
                     queryName={'popularOnRt'}
                 ></TopFilmsManager>
 
                 <TopFilmsManager
-                    tittle={'POPULAR STREAMING MOVIES'}
-                    to={'/browse?at=home&filmType=1&sortBy=view_count-Desc'}
+                    tittle={'ADULTS ONLY POPULAR TV SHOWS'}
+                    to={
+                        '/browse?at=home&filmType=2&sortBy=view_count-Desc&rating=PG-18'
+                    }
                     linkText={'View all'}
                     parameters={
-                        '?at=home&filmType=1&sortBy=view_count-Desc&page=0&size=10'
+                        '?&atStreaming=1&filmType=2&genres=&suitableFor=PG-18&streamId=&orderBy=view_count-Desc&size=10'
                     }
                     queryName={'popularStreaming'}
                 ></TopFilmsManager>
 
                 <TopFilmsManager
                     tittle={'NEW TV THIS WEEK'}
-                    to={
-                        '/browse?at=upcoming&filmType=all&sortBy=added_date-Desc'
-                    }
+                    to={'/browse?at=tv&filmType=all&sortBy=added_date-Desc'}
                     linkText={'View all'}
                     parameters={
-                        '?at=upcoming&filmType=all&sortBy=added_date-Desc&page=0&size=10'
+                        '?genres=&suitableFor=&streamId=&orderBy=added_date-Desc&size=10'
                     }
                     queryName={'newThisWeek'}
                 ></TopFilmsManager>
 
                 <TopFilmsManager
-                    tittle={'NEW & UPCOMING MOVIES IN THEATERS'}
-                    to={'/browse?at=upcoming&filmType=1'}
+                    tittle={'POPULAR MOVIES FOR ALL AGES'}
+                    to={
+                        '/browse?at=home&filmType=1&sortBy=view_count-Desc&rating=G'
+                    }
                     linkText={'View all'}
-                    parameters={'?at=upcoming&filmType=1&page=0&size=10'}
+                    parameters={
+                        '?&atStreaming=1&filmType=1&genres=&suitableFor=G&streamId=&orderBy=view_count-Desc&size=10'
+                    }
                     queryName={'newAndUpcomingAtTheaters'}
                 ></TopFilmsManager>
             </div>
 
             <TittleCard
                 tittle={'POPULAR MOVIES AT THEATERS'}
-                to={'/adasd'}
+                to={'/browse?at=theaters&filmType=all&sortBy=view_count-Desc'}
                 linkText={'View all'}
             ></TittleCard>
             <RegularCarouselManager
                 parameters={
-                    '?filmType=1&atTheaters=1&orderBy=view_count-asc&page=0&size=15'
+                    '?atTheaters=1&genres=&suitableFor=&streamId=&orderBy=view_count-Desc&size=15'
                 }
                 queryName={'popularMoviesAtTheaters'}
                 promiseName={'popularMoviesAtTheatersImages'}
@@ -98,12 +102,14 @@ function Home() {
 
             <TittleCard
                 tittle={'LATEST CERTIFIED FRESH MOVIES'}
-                to={'/adasd'}
+                to={
+                    '/browse?at=home&filmType=1&sortBy=average_super_rating-Desc'
+                }
                 linkText={'View all'}
             ></TittleCard>
             <RegularCarouselManager
                 parameters={
-                    '?filmType=1&orderBy=average_super_rating-desc,view_count-desc&page=0&size=15'
+                    '?&atStreaming=1&filmType=1&genres=&suitableFor=&streamId=&orderBy=average_super_rating-Desc&size=15'
                 }
                 queryName={'latestCertified'}
                 promiseName={'latestCertifiedImages'}
