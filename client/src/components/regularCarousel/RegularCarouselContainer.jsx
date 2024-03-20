8fdc14b75b6452ba71e357abcf0dc904c1b66442
Jorge Cobos <cobos1932@gmail.com>
2024-03-20 15:04:17 -0600

    edit breakpoints

--- client/src/components/regularCarousel/RegularCarouselContainer.jsx
+++ client/src/components/regularCarousel/RegularCarouselContainer.jsx
@@ -5,16 +5,16 @@ import Carousel from 'react-elastic-carousel'
 function RegularCarouselContainer({ data }) {
     const breakPointsMovie = [
         { width: 1, itemsToShow: 2, itemsToScroll: 2 },
-        { width: 408, itemsToShow: 3, itemsToScroll: 3 },
-        { width: 550, itemsToShow: 4, itemsToScroll: 3 },
-        { width: 672, itemsToShow: 5, itemsToScroll: 3 },
-        { width: 768, itemsToShow: 4, itemsToScroll: 4 },
+        { width: 400, itemsToShow: 3, itemsToScroll: 3 },
+        { width: 540, itemsToShow: 4, itemsToScroll: 3 },
+        { width: 694, itemsToShow: 5, itemsToScroll: 3 },
+        { width: 752, itemsToShow: 4, itemsToScroll: 3 },
         { width: 880, itemsToShow: 5, itemsToScroll: 3 },
         { width: 1070, itemsToShow: 6, itemsToScroll: 5 },
     ]
 
     return (
-        <div className="relative mt-2 mb-4">
+        <div className="relative mt-2 mb-6">
             <Carousel
                 disableArrowsOnEnd={true}
                 enableMouseSwipe={false}
