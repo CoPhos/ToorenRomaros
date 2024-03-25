60dc2f3288c7a8222da93060f4fc373bbdf9f6af
Jorge Cobos <cobos1932@gmail.com>
2024-03-25 17:23:55 -0600

    change notfound component message

--- client/src/components/routes/NotFound.jsx
+++ client/src/components/routes/NotFound.jsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom'
 
 function NotFound() {
     return (
-        <article className='p-[100px]'>
+        <article className="p-[100px]">
             <h1>Oops!</h1>
-            <p>Page Not Found</p>
+            <p>Page Not Implemented yet.</p>
+            <p>This is still a work in progress.</p>
             <div className="flex items-center justify-center">
-                <Link to="/" className='text-blue-600 hover:text-blue-800'>Visit Our Homepage</Link>
+                <Link to="/" className="text-blue-600 hover:text-blue-800">
+                    Visit Our Homepage
+                </Link>
             </div>
         </article>
     )
